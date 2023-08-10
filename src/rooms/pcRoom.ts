ee041a95a2d193aa0760dcbfae744f578cd8d315
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-10 22:23:57 +0200

    docs(rooms): split pcRoom into parts

--- src/rooms/pcRoom.ts
+++ src/rooms/pcRoom.ts
@@ -8,9 +8,16 @@ import { createComputer } from '@/prefabs/computer.js'
 import { createTable } from '@/prefabs/table.js'
 
 export const createPCRoom = async (gameStateManager: Entity) => {
-  const table = createTable({ position: new Vector3(600, -80, 500) })
+  // "normal state" parts
 
-  const computer = createComputer({ position: new Vector3(600, -81, 480) })
+  const normalStool = Entity.seatStool1
+  normalStool.position = new Vector3(620, 0, 430)
+  normalStool.withScript()
+  normalStool.script?.properties.push(Interactivity.off)
+
+  // ----------------------
+
+  // "after suicide" parts
 
   const tippedStool = Entity.seatStool1
   tippedStool.position = new Vector3(490, -43, 250)
@@ -21,11 +28,15 @@ export const createPCRoom = async (gameStateManager: Entity) => {
     return `objecthide ${tippedStool.ref} yes`
   })
 
-  const normalStool = Entity.seatStool1
-  normalStool.position = new Vector3(620, 0, 430)
-  normalStool.withScript()
-  normalStool.script?.properties.push(Interactivity.off)
+  const hangedGoblin = Entity.hangedGob
+  hangedGoblin.position = new Vector3(505, -120, 260)
+  hangedGoblin.orientation = new Rotation(0, MathUtils.degToRad(180 - 45), 0)
+  hangedGoblin.withScript()
+  hangedGoblin.script?.on('init', () => {
+    return `objecthide ${hangedGoblin.ref} yes`
+  })
 
+  // TODO: what to do with this game when found?
   const bigRigs = new PCGame({
     variant: 'big-rigs',
     position: new Vector3(540, 0, 290),
@@ -35,6 +46,14 @@ export const createPCRoom = async (gameStateManager: Entity) => {
     return `objecthide ${bigRigs.ref} yes`
   })
   bigRigs.script?.on('inventoryin', () => `sendevent player_found_a_game ${gameStateManager.ref} ${bigRigs.variant}`)
+
+  // ----------------------
+
+  // "general" parts
+
+  const table = createTable({ position: new Vector3(600, -80, 500) })
+
+  const computer = createComputer({ position: new Vector3(600, -81, 480) })
 
   const ambientLight = createLight({
     position: new Vector3(650, -200, 400),
@@ -46,14 +65,6 @@ export const createPCRoom = async (gameStateManager: Entity) => {
   const monitorLight = createLight({
     position: new Vector3(617, -113, 450),
     radius: 200,
-  })
-
-  const hangedGoblin = Entity.hangedGob
-  hangedGoblin.position = new Vector3(505, -120, 260)
-  hangedGoblin.orientation = new Rotation(0, MathUtils.degToRad(180 - 45), 0)
-  hangedGoblin.withScript()
-  hangedGoblin.script?.on('init', () => {
-    return `objecthide ${hangedGoblin.ref} yes`
   })
 
   const door = new LightDoor({
