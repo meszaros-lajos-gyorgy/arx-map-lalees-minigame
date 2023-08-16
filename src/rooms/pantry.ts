77b79c4a6e20fbab73e1dc79d1aa2b9040adf52a
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-17 00:55:43 +0200

    feat(rooms/pantry): add some junk

--- src/rooms/pantry.ts
+++ src/rooms/pantry.ts
@@ -1,5 +1,6 @@
 import { Entity, Rotation, Vector3 } from 'arx-level-generator'
 import { Label, Shadow } from 'arx-level-generator/scripting/properties'
+import { randomBetween } from 'arx-level-generator/utils/random'
 import { MathUtils } from 'three'
 import { PCGame, PCGameVariant } from '@/entities/PCGame.js'
 import { createTable } from '@/prefabs/table.js'
@@ -19,16 +20,35 @@ export const createPantry = async (gameStateManager: Entity, gameVariant: PCGame
   })
   barrel.script?.properties.push(Shadow.off)
 
-  const table1a = createTable({ position: new Vector3(552, -80, -172), hasShadow: true })
-  const table1b = createTable({ position: new Vector3(552, -160, -172), hasShadow: false })
-  const table1c = createTable({ position: new Vector3(552, -240, -172), hasShadow: false })
+  const lowestShelf = -80
+  const middleShelf = -160
+  const highestShelf = -240
 
-  const table2a = createTable({ position: new Vector3(862, -80, -172), hasShadow: true })
-  const table2b = createTable({ position: new Vector3(862, -160, -172), hasShadow: false })
-  const table2c = createTable({ position: new Vector3(862, -240, -172), hasShadow: false })
+  const table1a = createTable({ position: new Vector3(552, lowestShelf, -172), hasShadow: true })
+  const table1b = createTable({ position: new Vector3(552, middleShelf, -172), hasShadow: false })
+  const table1c = createTable({ position: new Vector3(552, highestShelf, -172), hasShadow: false })
 
-  const bathroomKey = Entity.key.at({ position: new Vector3(912, -160, -172) }).withScript()
+  const table2a = createTable({ position: new Vector3(862, lowestShelf, -172), hasShadow: true })
+  const table2b = createTable({ position: new Vector3(862, middleShelf, -172), hasShadow: false })
+  const table2c = createTable({ position: new Vector3(862, highestShelf, -172), hasShadow: false })
+
+  const bathroomKey = Entity.key
+    .at({ position: new Vector3(912, middleShelf, -172), orientation: new Rotation(0, MathUtils.degToRad(90), 0) })
+    .withScript()
   bathroomKey.script?.properties.push(new Label('[key--bathroom]'))
+
+  const ropes = [
+    Entity.rope.at({ position: new Vector3(580, middleShelf, -180 + randomBetween(-5, 5)) }),
+    Entity.rope.at({ position: new Vector3(500, middleShelf, -180 + randomBetween(-5, 5)) }),
+    Entity.rope.at({ position: new Vector3(760, lowestShelf, -180 + randomBetween(-5, 5)) }),
+  ]
+
+  const jar = new Entity({
+    src: 'items/movable/jar',
+    position: new Vector3(620, lowestShelf, -190),
+    orientation: new Rotation(0, MathUtils.degToRad(90), 0),
+  })
+  jar.withScript()
 
   return {
     meshes: [
@@ -39,7 +59,7 @@ export const createPantry = async (gameStateManager: Entity, gameVariant: PCGame
       ...table2b.meshes,
       ...table2c.meshes,
     ],
-    entities: [game, barrel, bathroomKey],
+    entities: [game, barrel, bathroomKey, ...ropes, jar],
     lights: [],
     zones: [],
     _: {
