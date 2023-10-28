a3e5a20c14fb8adc56c935086ad51bfe016e1bd8
Lajos Meszaros <m_lajos@hotmail.com>
2023-10-28 22:12:13 +0200

    feat(entities/PowerupRing): automatically add it to the player when the map starts

--- src/entities/PowerupRing.ts
+++ src/entities/PowerupRing.ts
@@ -1,6 +1,7 @@
 import { Color, Entity, EntityConstructorPropsWithoutSrc, EntityModel, Texture } from 'arx-level-generator'
 import { Sound } from 'arx-level-generator/scripting/classes'
-import { Label, Material } from 'arx-level-generator/scripting/properties'
+import { useDelay } from 'arx-level-generator/scripting/hooks'
+import { Label, Material, Variable } from 'arx-level-generator/scripting/properties'
 
 const equipRingSound = new Sound('equip_ring')
 
@@ -21,22 +22,35 @@ export class PowerupRing extends Entity {
 
     this.withScript()
 
-    this.script?.properties.push(Material.metal, new Label('[ring--powerup]'))
+    const propSilentEquip = new Variable('bool', 'silent_equip', true)
 
-    this.script?.on('init', () => {
-      return `
-        setobjecttype ring
-        setequip intelligence +2
-        halo -ocs ${Color.fromCSS('gold').lighten(50).toScriptColor()} 50
-      `
-    })
+    this.script?.properties.push(Material.metal, new Label('[ring--powerup]'), propSilentEquip)
 
-    this.script?.on('inventoryuse', () => {
-      return `equip player`
-    })
+    this.script
+      ?.on('init', () => {
+        const brightGold = Color.fromCSS('gold').lighten(50)
 
-    this.script?.on('equipin', () => {
-      return equipRingSound.play()
-    })
+        return `
+          setobjecttype ring
+          setequip intelligence +2
+          halo -ocs ${brightGold.toScriptColor()} 50
+        `
+      })
+      .on('initend', () => {
+        const { delay } = useDelay()
+        return `
+          equip player
+          ${delay(100)} set ${propSilentEquip.name} 0
+        `
+      })
+      .on('inventoryuse', () => `equip player`)
+      .on('equipin', () => {
+        return `
+          if (${propSilentEquip.name} == 1) {
+            accept
+          }
+          ${equipRingSound.play()}
+        `
+      })
   }
 }
