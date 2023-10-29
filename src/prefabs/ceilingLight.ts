db11f148a7e05fee1116fdb7441c4a4d737f8d4b
Lajos Meszaros <m_lajos@hotmail.com>
2023-10-29 19:01:15 +0100

    feat(ceilingLight): create a prefab for creating the lamp entity and light

---
+++ src/prefabs/ceilingLight.ts
@@ -0,0 +1,33 @@
+import { ArxLightFlags } from 'arx-convert/types'
+import { Color, Vector3 } from 'arx-level-generator'
+import { createLight } from 'arx-level-generator/tools'
+import { CeilingLamp } from '@/entities/CeilingLamp.js'
+
+type createCeilingLightProps = {
+  position: Vector3
+  radius: number
+  isOn: boolean
+}
+
+export const createCeilingLight = ({ position, radius, isOn }: createCeilingLightProps) => {
+  const entity = new CeilingLamp({ position, isOn })
+
+  const light = createLight({
+    position: position.clone().add(new Vector3(0, 50, 0)),
+    fallStart: 100,
+    radius,
+    intensity: 1.3,
+    color: Color.white,
+  })
+  // light.exFlicker: toArxColor(toRgba('#1f1f07')),
+  light.flags =
+    ArxLightFlags.SemiDynamic |
+    ArxLightFlags.Extinguishable |
+    ArxLightFlags.NoIgnit |
+    (isOn ? ArxLightFlags.None : ArxLightFlags.StartExtinguished)
+
+  return {
+    entities: [entity],
+    lights: [light],
+  }
+}
