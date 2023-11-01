7a0d45a75badd896e48c0ee2d03a63c06dbbfe85
Lajos Meszaros <m_lajos@hotmail.com>
2023-11-01 20:11:10 +0100

    feat(bathroom): load bathtub as an entity instead of part of the mesh

--- src/prefabs/bathtub.ts
+++ src/prefabs/bathtub.ts
@@ -1,10 +1,5 @@
-import { ArxMap, Polygon, Settings, Texture, Vector3, Vertex } from 'arx-level-generator'
+import { ArxMap, Settings, Texture, Vector3 } from 'arx-level-generator'
 import { Box3 } from 'three'
-
-const barrelTexture = new Texture({
-  filename: 'fixinter_barrel.jpg',
-  size: 128, // it's actally 128×256
-})
 
 export const createBathtub = async (
   { position, scale = 1 }: { position: Vector3; scale?: number },
@@ -12,43 +7,27 @@ export const createBathtub = async (
 ) => {
   const castleMap = await ArxMap.fromOriginalLevel(0, settings)
 
+  const barrelTexture = new Texture({
+    filename: 'fixinter_barrel.jpg',
+    size: 128, // it's actally 128×256
+  })
+
   const center = new Vector3(4703, 1775, 8654)
   const box = new Box3(
     center.clone().add(new Vector3(-200, -200, -200)),
     center.clone().add(new Vector3(200, 200, 200)),
   )
 
+  const adjustment = new Vector3(0, 50, 0)
   const inverseCenter = center.clone().multiplyScalar(-1)
 
-  const polygons = castleMap.polygons
-    .filter((polygon) => {
-      if (!polygon.isWithin(box)) {
-        return false
-      }
-
-      if (typeof polygon.texture === 'undefined') {
-        return false
-      }
-
-      return polygon.texture.equals(barrelTexture)
-    })
-    .map((_polygon) => {
-      _polygon.move(inverseCenter)
-      _polygon.move(new Vector3(0, 50, 0))
-      _polygon.scale(scale)
-
-      const polygon = new Polygon({
-        vertices: _polygon.vertices,
-        texture: _polygon.texture,
-        isQuad: _polygon.isQuad(),
-        flags: _polygon.flags,
-      })
-
-      polygon.move(position)
-      return polygon
-    })
-
-  return {
-    polygons,
-  }
+  return castleMap.polygons
+    .selectWithinBox(box)
+    .selectByTextures([barrelTexture])
+    .copy()
+    .moveToRoom1()
+    .move(inverseCenter)
+    .move(adjustment)
+    .scale(scale)
+    .move(position)
 }
