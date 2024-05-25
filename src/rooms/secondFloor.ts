e378a8eb31baaa959e7570152debc6389fad9e7e
Lajos Meszaros <m_lajos@hotmail.com>
2024-05-25 21:10:36 +0200

    feat(secondFloor): add invisible walls

--- src/rooms/secondFloor.ts
+++ src/rooms/secondFloor.ts
@@ -1,48 +1,98 @@
 import { ArxPolygonFlags } from 'arx-convert/types'
-import { Entity, Material, Settings, Texture, Vector3 } from 'arx-level-generator'
+import { Entity, Material, Settings, Texture, TextureOrMaterial, Vector3 } from 'arx-level-generator'
 import { createPlaneMesh } from 'arx-level-generator/prefabs/mesh'
 import { MathUtils, Vector2 } from 'three'
 import { RoomContents } from '@/types.js'
 
-const transparentGlass = Material.fromTexture(Texture.glassGlass01, {
+const glass = Material.fromTexture(Texture.glassGlass01, {
   opacity: 70,
   flags: ArxPolygonFlags.DoubleSided | ArxPolygonFlags.NoShadow,
 })
 
-const createFenceAt = (center: Vector3, offset: Vector2, isRotated: boolean = false) => {
-  const railing = createPlaneMesh({
-    size: new Vector2(600, 100),
-    texture: transparentGlass,
-    tileUV: true,
-  })
+const invisibleWall = Material.fromTexture(Texture.alpha, {
+  flags: ArxPolygonFlags.NoShadow,
+})
 
-  railing.translateX(center.x + offset.x)
-  railing.translateY(center.y)
-  railing.translateZ(center.z + offset.y)
+const createPlaneAt = ({
+  texture,
+  size,
+  position,
+  angleY,
+}: {
+  texture: TextureOrMaterial
+  size: Vector2
+  position: Vector3
+  angleY: number
+}) => {
+  const plane = createPlaneMesh({ size, texture, tileUV: true })
 
-  railing.rotateX(MathUtils.degToRad(90))
+  plane.translateX(position.x)
+  plane.translateY(position.y - size.y / 2)
+  plane.translateZ(position.z)
 
-  if (isRotated) {
-    railing.rotateZ(MathUtils.degToRad(90))
-  }
+  plane.rotateX(MathUtils.degToRad(90))
+  plane.rotateZ(MathUtils.degToRad(angleY))
 
-  return railing
+  return plane
 }
 
 export const createSecondFloor = async (settings: Settings, gameStateManager: Entity): Promise<RoomContents> => {
-  const roomOrigin = new Vector3(-2450, -470, 0)
+  const roomOrigin = new Vector3(-2450, -400, 0)
 
-  const railing1 = createFenceAt(roomOrigin, new Vector2(0, -300))
-  const railing2 = createFenceAt(roomOrigin, new Vector2(0, 300))
-  const railing3 = createFenceAt(roomOrigin, new Vector2(-300, 0), true)
-  const railing4 = createFenceAt(roomOrigin, new Vector2(300, 0), true)
+  const railing1 = createPlaneAt({
+    texture: glass,
+    size: new Vector2(600, 100),
+    position: roomOrigin.clone().add(new Vector3(0, -20, -300)),
+    angleY: 0,
+  })
+  const railing2 = createPlaneAt({
+    texture: glass,
+    size: new Vector2(600, 100),
+    position: roomOrigin.clone().add(new Vector3(0, -20, 300)),
+    angleY: 180,
+  })
+  const railing3 = createPlaneAt({
+    texture: glass,
+    size: new Vector2(600, 100),
+    position: roomOrigin.clone().add(new Vector3(-300, -20, 0)),
+    angleY: -90,
+  })
+  const railing4 = createPlaneAt({
+    texture: glass,
+    size: new Vector2(600, 100),
+    position: roomOrigin.clone().add(new Vector3(300, -20, 0)),
+    angleY: 90,
+  })
 
-  // TODO: add invisible wall behind glass to prevent player jumping over
+  const blocker1 = createPlaneAt({
+    texture: invisibleWall,
+    size: new Vector2(598, 200),
+    position: roomOrigin.clone().add(new Vector3(0, 0, -299)),
+    angleY: 0,
+  })
+  const blocker2 = createPlaneAt({
+    texture: invisibleWall,
+    size: new Vector2(598, 200),
+    position: roomOrigin.clone().add(new Vector3(0, 0, 299)),
+    angleY: 180,
+  })
+  const blocker3 = createPlaneAt({
+    texture: invisibleWall,
+    size: new Vector2(598, 200),
+    position: roomOrigin.clone().add(new Vector3(-299, 0, 0)),
+    angleY: -90,
+  })
+  const blocker4 = createPlaneAt({
+    texture: invisibleWall,
+    size: new Vector2(598, 200),
+    position: roomOrigin.clone().add(new Vector3(299, 0, 0)),
+    angleY: 90,
+  })
 
   // TODO: add corner poles to support glass railing
 
   return {
-    meshes: [railing1, railing2, railing3, railing4],
+    meshes: [railing1, railing2, railing3, railing4, blocker1, blocker2, blocker3, blocker4],
     entities: [],
     lights: [],
     zones: [],
